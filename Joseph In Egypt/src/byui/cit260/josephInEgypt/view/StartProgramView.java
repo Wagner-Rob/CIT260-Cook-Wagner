f3bb575379667140f5873ade4a8f11fc832b996f
rbtwa <rbtwa@Rob>
2017-10-28 22:16:22 -0600

    Week 7

--- Joseph In Egypt/src/byui/cit260/josephInEgypt/view/StartProgramView.java
+++ Joseph In Egypt/src/byui/cit260/josephInEgypt/view/StartProgramView.java
@@ -9,6 +9,16 @@ package byui.cit260.josephInEgypt.view;
  *
  * @author rbtwa
  */
+//Note:
+//Ran out of time on this one, I started on it Wednesday, and have put over 8
+//hours of coding into it, which has helped tremendously in expanding my Java
+//knowledge, however, since I need to get it turned in now, and I wasn't able to 
+//get all of the info I wanted to, I'm turning in what I have for now, and will
+//work on completing this next week if I have time to do it.
+//Also note: We're down a team member since Kathlyne has withdrawn from this course.
+//The reading took about 4-6 hours, and the other assignments about 1-2, so coding
+//isn't the only thing I've been working on this week, but the syntax feels like
+//it's starting to come together now.
 public class StartProgramView {
 
     public static void displayStartProgramView() {
@@ -24,12 +34,44 @@ public class StartProgramView {
         //do requested action and display next view
         done = this.doAction(playersName);
     } while (!done);
-    
     }
     
     private String getPlayersName() {
-        System.out.println("n/*** getPlayersName() function called ***");
-        return "Joe";
+        
+        Scanner keyboard = new Scanner(System.in); //get infile for keyboard
+        String value = ""; //value to be returned
+        boolean valid = false; //initialize to not valid
+        
+        while(!valid) {//loop while an invalid value is entered
+            System.out.println("\n" + this.promptMessage);
+            
+            value = keyboard.nextLine(); //get next line typed on keyboard
+            value = value.trim(); //trim off leading and trailing blanks
+            
+            if (value.length() < 1) {//if value is blank...
+                System.out.println("\nInvalid value: value cannot be blank");
+                continue;
+            }
+            break; //end the loop
+            }
+        return value; //return the value entered
+        }
+        /*
+        WHILE valid value has not been entered
+            DISPLAY promptMessage
+            GET the value entered from keyboard
+            Trim front and trailing blanks off of the name
+        
+            IF the length of the value is blank THEN
+                DISPLAY "invalid value: The value cannot be blank"
+                CONTINUE
+            ENDIF
+        
+            BREAK
+        
+        ENDWHILE
+        RETURN name
+        */
     }
 
     private boolean doAction(String playersName) {
@@ -61,6 +103,5 @@ public class StartProgramView {
       + "\n                                                               *"
       + "\n****************************************************************"   
        );
-    }
-}
+}    
 
